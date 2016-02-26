42bbe85daa1bf33b6339ca021ae2ca2818c1918d
fneu <fabian@neuschmidt.de>
2016-02-26 17:56:48 +0100

    orca: Convert input files to tiff

--- ocra.py
+++ ocra.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python2
+import CliParsing
+import Converting
+import glob
 import shutil
 import tempfile
 
 if __name__ == "__main__":
     temp_dir = tempfile.mkdtemp(prefix="ocra_tmp")
     try:
-        pass
+        args = CliParsing.cli_args()
+        file_list = Converting.pdf_to_tiff(args['f'], temp_dir)
 
     finally:
         shutil.rmtree(temp_dir)
