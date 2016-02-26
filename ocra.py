cae60f83aafe097a9a406d2276a34ffff1307932
fneu <fabian@neuschmidt.de>
2016-02-26 17:05:42 +0100

    ocra: Cleanly provide and close a tempdir

--- ocra.py
+++ ocra.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python2
+import shutil
+import tempfile
 
 if __name__ == "__main__":
-    pass
+    temp_dir = tempfile.mkdtemp(prefix="ocra_tmp")
+    try:
+        pass
+
+    finally:
+        shutil.rmtree(temp_dir)
